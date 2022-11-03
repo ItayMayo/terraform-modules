ea7ba80feafa96818d5ddce37e24f9749c9d7f33
ItayMayo <itaymayo02@gmail.com>
2022-11-03 14:23:32 +0200

    ACR: config.tf - Added default variable values and descriptions.

--- ACR/config.tf
+++ ACR/config.tf
@@ -1,19 +1,27 @@
 variable "acr_name" {
-  type = string
+  type        = string
+  description = "Name of the Container Registry."
 }
 
 variable "acr_sku" {
-  type = string
+  type        = string
+  description = "Container Registry SKU name."
 }
 
 variable "admin_enabled" {
-  type = bool
+  default     = false
+  type        = bool
+  description = "Enable admin account."
 }
 
 variable "public_network_access_enabled" {
-  type = bool
+  default     = true
+  type        = bool
+  description = "Enable ACR public network access."
 }
 
 variable "data_endpoint_enabled" {
-  type = bool
+  default     = false
+  type        = bool
+  description = "Enable ACR Data Endpoint."
 }
