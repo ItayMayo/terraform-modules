9e9d2d7701ab01a8c009af8c0b2587b73db1144f
ItayMayo <itaymayo02@gmail.com>
2022-11-03 17:52:38 +0200

    analytics-workspace: variables.tf - Added tags variable, added variable descriptiosn.

--- analytics_workspace/variables.tf
+++ analytics_workspace/variables.tf
@@ -1,9 +1,15 @@
-variable "resource_location" {
+variable "location" {
   type        = string
   description = "Location of the resource."
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the parent Resource Group."
 }
 
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "Tags assigned to the resource."
+}
