4380fb64e458a59be04b44f1d4427a0c1f25c935
ItayMayo <itaymayo02@gmail.com>
2022-11-03 17:43:26 +0200

    FirewallRules: Removed now redundant module.

--- FirewallRules/variables.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  network_rules_provided     = var.rule_collections["network_rule_collections"] != null
-  application_rules_provided = var.rule_collections["application_rule_collections"] != null
-  nat_rules_provided         = var.rule_collections["nat_rule_collections"] != null
-}
