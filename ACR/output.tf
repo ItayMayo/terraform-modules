2ce12fc34000630cfc7d1059689b4329e7b28d4f
ItayMayo <itaymayo02@gmail.com>
2022-11-05 18:54:59 +0200

    acr: output.tf - Added endpoint, dns object output.

--- ACR/output.tf
+++ ACR/output.tf
@@ -1,11 +1,25 @@
 output "acr_id" {
-  value = azurerm_container_registry.acr.id
+  value       = azurerm_container_registry.acr.id
+  description = "ACR resource id."
 }
 
 output "acr_name" {
-  value = azurerm_container_registry.acr.name
+  value       = azurerm_container_registry.acr.name
+  description = "ACR resource name."
 }
 
 output "acr_object" {
-  value = azurerm_container_registry.acr
+  value       = azurerm_container_registry.acr
+  description = "ACR resource object."
+}
+
+output "endpoint_object" {
+  value       = try(azurerm_private_endpoint.endpoint, null)
+  description = "Storage Account Private Endpoint resource object."
+}
+
+output "private_dns_object" {
+  value       = try(module.storage_account_private_dns["acr_dns"].dns_object, null)
+  description = "Storage Account Private DNS resource object."
+
 }
