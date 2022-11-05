7ef6d7c8342523759085b1df12f52c52e73fa12f
ItayMayo <itaymayo02@gmail.com>
2022-11-05 16:14:20 +0200

    vnet: config.tf - Added variable descriptions. Added subnets variable.

--- Network/VNet/config.tf
+++ Network/VNet/config.tf
@@ -1,11 +1,24 @@
-variable "name" {
-  type = string
+variable "vnet_name" {
+  type        = string
+  description = "Name of the vnet."
 }
 
 variable "address_space" {
-  type = list(string)
+  type        = list(string)
+  description = "VNet Address Space. Usually has /16 CIDR."
 }
 
 variable "dns_servers" {
-  type = list(string)
+  default     = null
+  type        = list(string)
+  description = "Optional. DNS servers to be associated with this Virtual Network.."
+}
+
+variable "subnets" {
+  type = list(object({
+    subnet_name      = string
+    address_prefixes = list(string)
+  }))
+
+  description = "List of subnets to be associated with this Virtual Network."
 }
