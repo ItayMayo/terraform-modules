641216e85419f30d32683d37b5e68875a8246270
ItayMayo <itaymayo02@gmail.com>
2022-11-05 18:34:48 +0200

    private-dns: output.tf - Added descriptions.

--- private-dns/output.tf
+++ private-dns/output.tf
@@ -1,11 +1,14 @@
 output "dns_id" {
-  value = azurerm_private_dns_zone.private_dns.id
+  value       = azurerm_private_dns_zone.private_dns.id
+  description = "Private DNS resource id."
 }
 
 output "dns_name" {
-  value = azurerm_private_dns_zone.private_dns.name
+  value       = azurerm_private_dns_zone.private_dns.name
+  description = "Private DNS resource name."
 }
 
 output "dns_object" {
-  value = azurerm_private_dns_zone.private_dns
+  value       = azurerm_private_dns_zone.private_dns
+  description = "Private DNS resource object."
 }
