3bf2a20726f573b2668c7d08eb28158879299504
ItayMayo <itaymayo02@gmail.com>
2022-11-05 18:34:34 +0200

    private-dns: main.tf - Added tags property.

--- private-dns/main.tf
+++ private-dns/main.tf
@@ -1,12 +1,7 @@
 resource "azurerm_private_dns_zone" "private_dns" {
   name                = var.zone_name
   resource_group_name = var.resource_group_name
-
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
+  tags                = var.tags
 }
 
 resource "azurerm_private_dns_a_record" "a_record" {
@@ -19,17 +14,16 @@ resource "azurerm_private_dns_a_record" "a_record" {
   records             = each.value["records"]
 }
 
+locals {
+  vnet_link_prefix = "dns_link_"
+}
+
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_vnet_link" {
   for_each = var.vnet_ids
 
-  name                  = "dns_link_${each.key}"
+  name                  = "${local.dns_link_prefix}${each.key}"
   resource_group_name   = var.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns.name
   virtual_network_id    = each.value
-
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
+  tags                  = var.tags
 }
