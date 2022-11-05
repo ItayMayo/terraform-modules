9578050de7110c129c34058a7776f7bf31fc5c14
ItayMayo <itaymayo02@gmail.com>
2022-11-05 18:35:07 +0200

    private-dns: variables.tf - Added tags, added descriptions.

--- private-dns/variables.tf
+++ private-dns/variables.tf
@@ -1,3 +1,10 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the parent Resource Group."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "Tags assigned to the resource."
 }
