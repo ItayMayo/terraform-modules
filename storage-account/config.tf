6f6d60457645c04e9b8cafe9395d7b2333d67ce4
ItayMayo <itaymayo02@gmail.com>
2022-11-05 18:28:31 +0200

    storage-account: config.tf - Added variable descriptions. Added variables related to SA private endpoint, private dns.

--- storage-account/config.tf
+++ storage-account/config.tf
@@ -1,28 +1,41 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "Storage Accuont name."
 }
 
 variable "account_tier" {
-  type = string
+  default     = "Standard"
+  type        = string
+  description = "Storage Accuont tier. Default: Standard."
 }
 
 variable "account_kind" {
-  type = string
+  default     = "StorageV2"
+  type        = string
+  description = "Storage Account kind. Default: StorageV2."
 }
 
 variable "replication_type" {
-  type = string
+  default     = "GRS"
+  type        = string
+  description = "Storage Account replication type. Default: GRS."
 }
 
 variable "allow_nested_items_to_be_public" {
-  type = bool
+  default     = false
+  type        = bool
+  description = "Allow Storage Account nested items to be accessible over the public network. Default: false."
 }
 
 variable "public_network_access_enabled" {
-  type = bool
+  default     = false
+  type        = bool
+  description = "Enable Storage Account public network access. Default: false."
 }
 
 variable "network_rules" {
+  default = null
+
   type = list(object({
     default_action             = string
     bypass                     = list(string)
@@ -34,4 +47,30 @@ variable "network_rules" {
       endpoint_tenant_id   = string
     })
   }))
+
+  description = "Optional. Storage Account Network Rules."
+}
+
+variable "private_endpoint_subnet_id" {
+  default     = null
+  type        = string
+  description = "Optional. ID of the subnet in which to create a private endpoint for this Storage Account."
+}
+
+variable "create_private_dns" {
+  default     = false
+  type        = bool
+  description = "Optional. Create Private DNS associated with the Storage Account. Requires Private Endpoint. Default: false."
+}
+
+variable "private_dns_zone_name" {
+  default     = "azurestorage.io"
+  type        = string
+  description = "Optional. Name of a Private DNS zone to be associated with the storage account. Default: azurestorage.io."
+}
+
+variable "private_dns_vnets" {
+  default     = null
+  type        = list(string)
+  description = "List of Virtual Networks to associate with the private endpoint. Required when create_private_dns is enabled."
 }
