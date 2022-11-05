326ca7c7e03aee2b605c1678f770d23fced58f3f
ItayMayo <itaymayo02@gmail.com>
2022-11-05 16:20:22 +0200

    network-interface: main.tf - Added tags property.

--- Network/NetworkInterface/main.tf
+++ Network/NetworkInterface/main.tf
@@ -1,18 +1,13 @@
 resource "azurerm_network_interface" "nic" {
   name                = var.name
   resource_group_name = var.resource_group_name
-  location            = var.resource_location
+  location            = var.location
+  tags                = var.tags
 
   ip_configuration {
     name                          = var.ip_configuration_name
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = var.private_ip_address_allocation
-  }
-
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
   }
 }
 
