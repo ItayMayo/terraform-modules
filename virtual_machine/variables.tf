74a7f0bd7b1c412622349b9ab9d5eebf42b9697a
ItayMayo <itaymayo02@gmail.com>
2022-11-05 17:55:23 +0200

    virtual_machine: variables.tf - Added tags.

--- virtual_machine/variables.tf
+++ virtual_machine/variables.tf
@@ -1,17 +1,21 @@
-locals {
-  identity_provided = var.identity != null
-}
-
-variable "resource_location" {
+variable "location" {
   type        = string
   description = "Location of the resource."
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "Name of the parent Resource Group."
 }
 
 variable "log_workspace_id" {
-  default = null
-  type    = string
+  default     = null
+  type        = string
+  description = "ID of the log analytics workspace where logs should be sent to."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "Tags assigned to the resource."
 }
