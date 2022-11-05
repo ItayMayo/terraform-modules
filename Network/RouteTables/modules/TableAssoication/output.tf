ab69c14c34c115c0877564523bc97ec85265a904
ItayMayo <itaymayo02@gmail.com>
2022-11-05 17:07:10 +0200

    Network: Removed redundant RouteTables module.

--- Network/RouteTables/modules/TableAssoication/output.tf
+++
@@ -1,7 +0,0 @@
-output "association_id" {
-    value = azurerm_subnet_route_table_association.association.id
-}
-
-output "association_object" {
-    value = azurerm_subnet_route_table_association.association
-}
