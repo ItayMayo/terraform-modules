8ca1915b14ed876bb4059fab0e8380354dbbc5db
ItayMayo <itaymayo02@gmail.com>
2022-11-05 17:57:25 +0200

    virtual_machine: output.tf - Added vm_nic_object output module.

--- virtual_machine/output.tf
+++ virtual_machine/output.tf
@@ -12,3 +12,8 @@ output "virtual_machine_object" {
   value       = azurerm_linux_virtual_machine.vm
   description = "Virtual Machine resource object."
 }
+
+output "vm_nic_object" {
+  value       = module.vm_network_interface.nic_object
+  description = "Network Interface resource object associated with the Virtual Machine."
+}
