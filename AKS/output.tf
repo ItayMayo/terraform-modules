40ac33a66566cb3bbca9fc61c65f9cfbcbfea143
ItayMayo <itaymayo02@gmail.com>
2022-11-05 19:07:40 +0200

    aks: output.tf - Added descriptions, added endpoint, dns exports.

--- AKS/output.tf
+++ AKS/output.tf
@@ -1,21 +1,36 @@
 output "cluster_id" {
-  value = azurerm_kubernetes_cluster.cluster.id
+  value       = azurerm_kubernetes_cluster.cluster.id
+  description = "AKS cluster resource id."
 }
 
 output "cluster_name" {
-  value = azurerm_kubernetes_cluster.cluster.name
+  value       = azurerm_kubernetes_cluster.cluster.name
+  description = "AKS cluster resource name."
 }
 
 output "cluster_object" {
-  value = azurerm_kubernetes_cluster.cluster
+  value       = azurerm_kubernetes_cluster.cluster
+  description = "AKS cluster resource object."
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
-  sensitive = true
+  value       = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+  sensitive   = true
+  description = "AKS cluster resource client certificate."
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.cluster.kube_config_raw
-  sensitive = true
+  value       = azurerm_kubernetes_cluster.cluster.kube_config_raw
+  sensitive   = true
+  description = "AKS cluster resource kube config."
+}
+
+output "endpoint_object" {
+  value       = try(azurerm_private_endpoint.endpoint, null)
+  description = "AKS Private Endpoint resource object."
+}
+
+output "private_dns_object" {
+  value       = try(module.storage_account_private_dns["aks_dns"].dns_object, null)
+  description = "AKS Private DNS resource object."
 }
