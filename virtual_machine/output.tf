ef877561fe0d069aa35a31023d4dd053ae708b7b
ItayMayo <itaymayo02@gmail.com>
2022-11-05 17:54:53 +0200

    virtual_machine: output.tf - Added descriptions.

--- virtual_machine/output.tf
+++ virtual_machine/output.tf
@@ -1,11 +1,14 @@
 output "virtual_machine_id" {
-  value = azurerm_linux_virtual_machine.vm.id
+  value       = azurerm_linux_virtual_machine.vm.id
+  description = "Virtual Machine resource id."
 }
 
 output "virtual_machine_name" {
-  value = azurerm_linux_virtual_machine.vm.name
+  value       = azurerm_linux_virtual_machine.vm.name
+  description = "Virtual Machine resource name."
 }
 
 output "virtual_machine_object" {
-  value = azurerm_linux_virtual_machine.vm
+  value       = azurerm_linux_virtual_machine.vm
+  description = "Virtual Machine resource object."
 }
