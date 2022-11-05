e24c83f4a69084b8f7c208fae6046e8c2d4eac51
ItayMayo <itaymayo02@gmail.com>
2022-11-05 16:20:38 +0200

    network-interface: output.tf - Added descriptions.

--- Network/NetworkInterface/output.tf
+++ Network/NetworkInterface/output.tf
@@ -1,11 +1,14 @@
 output "nic_id" {
-  value = azurerm_network_interface.nic.id
+  value       = azurerm_network_interface.nic.id
+  description = "ID of the network interface resource."
 }
 
 output "nic_name" {
-  value = azurerm_network_interface.nic.name
+  value       = azurerm_network_interface.nic.name
+  description = "Name of the network interface resource."
 }
 
 output "nic_object" {
-  value = azurerm_network_interface.nic
+  value       = azurerm_network_interface.nic
+  description = "Object of the network interface resource."
 }
