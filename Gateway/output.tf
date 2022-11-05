fc89896acc5300a79e241b148f6aed78a8c57f8b
ItayMayo <itaymayo02@gmail.com>
2022-11-05 16:00:29 +0200

    Gateway: output.tf - Added descriptions.

--- Gateway/output.tf
+++ Gateway/output.tf
@@ -1,11 +1,14 @@
 output "gateway_id" {
-  value = azurerm_virtual_network_gateway.vng.id
+  value       = azurerm_virtual_network_gateway.vng.id
+  description = "ID of the VNG resource."
 }
 
 output "gateway_name" {
-  value = azurerm_virtual_network_gateway.vng.name
+  value       = azurerm_virtual_network_gateway.vng.name
+  description = "Name of the VNG resource."
 }
 
 output "gateway_object" {
-  value = azurerm_virtual_network_gateway.vng
+  value       = azurerm_virtual_network_gateway.vng
+  description = "Object of the VNG resource."
 }
