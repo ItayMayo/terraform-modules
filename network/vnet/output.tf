b0e6e424957d3bf97e1495c432c0bed1d9951cb4
ItayMayo <itaymayo02@gmail.com>
2022-11-06 19:21:17 +0200

    vnet: output.tf: Removed subnet exported variables.

--- network/vnet/output.tf
+++ network/vnet/output.tf
@@ -12,13 +12,3 @@ output "vnet_object" {
   value       = azurerm_virtual_network.vnet
   description = "Object of the Virtual Network resource."
 }
-
-output "subnet_ids" {
-  value       = { for subnet in azurerm_subnet.subnets : subnet.name => subnet.id }
-  description = "List of subnet resource ids associated with the Virtual Network resource."
-}
-
-output "subnet_objects" {
-  value       = azurerm_subnet.subnets
-  description = "Objects of the Subnet resources associated with the Virtual Network resource."
-}
