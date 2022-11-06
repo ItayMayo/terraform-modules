c4a11a3db6bd8b60402e7f3bc76a5b368390250a
ItayMayo <itaymayo02@gmail.com>
2022-11-06 19:20:58 +0200

    vnet: config.tf - Merged variables.tf with config.tf. Removed subnet variables.

--- network/vnet/config.tf
+++ network/vnet/config.tf
@@ -1,3 +1,25 @@
+variable "location" {
+  type        = string
+  description = "Location of the resource."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the parent Resource Group."
+}
+
+variable "log_workspace_id" {
+  default     = null
+  type        = string
+  description = "ID of the log analytics workspace where logs should be sent to."
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "Tags assigned to the resource."
+}
+
 variable "vnet_name" {
   type        = string
   description = "Name of the vnet."
@@ -12,15 +34,4 @@ variable "dns_servers" {
   default     = null
   type        = list(string)
   description = "Optional. DNS servers to be associated with this Virtual Network.."
-}
-
-variable "subnets" {
-  type = map(object({
-    subnet_name      = string
-    address_prefixes = list(string)
-    nsg_id           = string
-    route_table_id   = string
-  }))
-
-  description = "Map of subnets to be associated with this Virtual Network."
 }
