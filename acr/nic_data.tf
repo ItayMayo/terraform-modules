2fe01a655721d65dec7291d1cbf4ec1b06a6dd7b
ItayMayo <itaymayo02@gmail.com>
2022-11-07 14:08:53 +0200

    acr: main.tf - Replaced for_each value from list to map.

--- acr/nic_data.tf
+++ acr/nic_data.tf
@@ -1,6 +1,6 @@
 data "azurerm_network_interface" "acr_nic" {
   for_each = local.create_private_endpoint ? { acr = "acr" } : {}
 
-  name                = azurerm_private_endpoint.endpoint[0].network_interface[0]["name"]
+  name                = azurerm_private_endpoint.endpoint["acr_endpoint"].network_interface[0]["name"]
   resource_group_name = var.resource_group_name
 }
