abaf95c2c0f290622ba91aef5587ae0d51994cd3
ItayMayo <itaymayo02@gmail.com>
2022-11-07 09:54:51 +0200

    subnet: main.tf - Added null checks.

--- network/subnet/main.tf
+++ network/subnet/main.tf
@@ -6,12 +6,24 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = var.address_prefixes
 }
 
+locals {
+  nsg_provided = var.nsg_id != null
+}
+
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  for_each = local.nsg_provided ? [1] : []
+
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = var.nsg_id
 }
 
+locals {
+  route_table_provided = var.route_table_id != null
+}
+
 resource "azurerm_subnet_route_table_association" "route_table_association" {
+  for_each = local.route_table_provided ? [1] : []
+
   subnet_id      = azurerm_subnet.subnet.id
   route_table_id = var.route_table_id
 }
