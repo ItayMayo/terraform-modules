22c3333ca2f440acb4af30d7ee07d8b6c8bc408c
ItayMayo <itaymayo02@gmail.com>
2022-11-09 19:24:23 +0200

    acr: main.tf - Folded data block from file into main.tf.

--- acr/main.tf
+++ acr/main.tf
@@ -39,6 +39,13 @@ locals {
   dns_record_ttl     = 3600
 }
 
+data "azurerm_network_interface" "acr_nic" {
+  for_each = local.create_private_endpoint ? { acr = "acr" } : {}
+
+  name                = azurerm_private_endpoint.endpoint["acr_endpoint"].network_interface[0]["name"]
+  resource_group_name = var.resource_group_name
+}
+
 module "acr-private-dns" {
   source = "github.com/ItayMayo/terraform-modules//private-dns"
 
--- acr/nic_data.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_network_interface" "acr_nic" {
-  for_each = local.create_private_endpoint ? { acr = "acr" } : {}
-
-  name                = azurerm_private_endpoint.endpoint["acr_endpoint"].network_interface[0]["name"]
-  resource_group_name = var.resource_group_name
-}
