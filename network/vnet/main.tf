54b1f91261773c614ece0632722bfece2dd67619
ItayMayo <itaymayo02@gmail.com>
2022-11-14 11:40:07 +0200

    vnet: main.tf - Added network-security-groups module.

--- network/vnet/main.tf
+++ network/vnet/main.tf
@@ -2,6 +2,20 @@
 * # Virtual Network Module
 */
 
+module "network-security-groups" {
+  source = "github.com/ItayMayo/terraform-modules//network/security-group"
+
+  for_each = local.security_groups
+
+  nsg_name            = each.key
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  nsg_security_rules = each.value
+
+  tags = var.tags
+}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   resource_group_name = var.resource_group_name
@@ -26,19 +40,20 @@ module "subnets" {
   route_table_id       = each.value["route_table_id"]
 
   depends_on = [
-    azurerm_virtual_network.vnet
+    azurerm_virtual_network.vnet,
+    module.network-security-groups
   ]
 }
 
 locals {
-  diagnostics_name   = "${var.vnet_name}-vnet-diagnostics"
-  target_resource_id = azurerm_virtual_network.vnet.id
+  diagnostics_name               = "${var.vnet_name}-vnet-diagnostics"
+  target_resource_id             = azurerm_virtual_network.vnet.id
   diagnostics_workspace_provided = var.log_workspace_id != null
 }
 
 module "diagnostics" {
-  source = "github.com/ItayMayo/terraform-modules//diagnostic-settings"
-    for_each = local.diagnostics_workspace_provided ? {"1": "1"} : {}
+  source   = "github.com/ItayMayo/terraform-modules//diagnostic-settings"
+  for_each = local.diagnostics_workspace_provided ? { "1" : "1" } : {}
 
   name                       = local.diagnostics_name
   target_resource_id         = local.target_resource_id
