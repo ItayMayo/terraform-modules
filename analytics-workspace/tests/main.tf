2a6a28e5fa844fc34ae1518110a63fb4cfb3b6ce
ItayMayo <itaymayo02@gmail.com>
2022-11-14 19:43:12 +0200

    analytics-workspace: Added tests to spin up a test environment with a log analytics workspace.

---
+++ analytics-workspace/tests/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "test-rg" {
+  name     = "itaym-acr-test"
+  location = "West Europe"
+}
+
+locals {
+  work_vnet_name   = "test-vnet"
+  work_subnet_name = "default"
+}
+
+module "log-analytics-workspace" {
+  source = "../"
+
+  name = "test-log-analytics"
+
+  resource_group_name = azurerm_resource_group.test-rg.name
+  location            = "westeurope"
+
+  depends_on = [
+    azurerm_resource_group.test-rg
+  ]
+}
+
