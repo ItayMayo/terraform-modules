935ca9fe3c9f79fba9ed138f249f714997285025
ItayMayo <itaymayo02@gmail.com>
2022-11-15 15:12:42 +0200

    virtual-machine: main.tf - Updated main to make use of the new modules. A

--- virtual-machine/main.tf
+++ virtual-machine/main.tf
@@ -22,51 +22,29 @@ module "vm-network-interface" {
   tags = var.tags
 }
 
-locals {
-  identity_provided = var.identity != null
-}
+module "windows-vm" {
+  source = "./windows-vm"
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
+  for_each = contains(["Windows"], var.vm_os_name) ? { vm = "Windows" } : {}
+
   resource_group_name = var.resource_group_name
   location            = var.location
+  log_workspace_id    = var.log_workspace_id
 
-  size                            = var.vm_size
-  admin_username                  = var.vm_admin_username
-  admin_password                  = var.vm_admin_password
-  disable_password_authentication = var.vm_disable_password_authentication
+  vm_name = var.vm_name
+  vm_size = var.vm_size
 
-  network_interface_ids = [module.vm-network-interface.id]
+  vm_admin_username = var.vm_admin_username
+  vm_admin_password = var.vm_admin_password
 
-  os_disk {
-    caching              = var.os_disk_caching
-    storage_account_type = var.storage_account_type
-  }
+  vm_nic_id = module.vm-network-interface.id
 
-  source_image_reference {
-    publisher = var.vm_source_image_reference["publisher"]
-    offer     = var.vm_source_image_reference["offer"]
-    sku       = var.vm_source_image_reference["sku"]
-    version   = var.vm_source_image_reference["version"]
-  }
+  vm_source_image_reference = var.vm_source_image_reference
+  identity                  = var.identity
 
-  dynamic "admin_ssh_key" {
-    for_each = var.vm_disable_password_authentication ? [var.vm_admin_ssh_key] : []
-
-    content {
-      username   = admin_ssh_key.value["username"]
-      public_key = admin_ssh_key.value["public_key"]
-    }
-  }
-
-  dynamic "identity" {
-    for_each = local.identity_provided ? [var.identity] : []
-
-    content {
-      type         = identity.value["type"]
-      identity_ids = identity.value["identity_ids"]
-    }
-  }
+  storage_account_type = var.storage_account_type
+  os_disk_caching      = var.os_disk_caching
+  disk_sizes_in_gb     = var.disk_sizes_in_gb
 
   tags = var.tags
 
@@ -75,56 +53,35 @@ resource "azurerm_linux_virtual_machine" "vm" {
   ]
 }
 
-locals {
-  create_additional_disks           = !contains(var.disk_sizes_in_gb, -1)
-  managed_disk_name_prefix          = "${var.vm_name}-managed-disk"
-  managed_disk_storage_account_type = "Standard_LRS"
-  managed_disk_create_option        = "Empty"
-}
+module "linux-vm" {
+  source = "./linux-vm"
 
-resource "azurerm_managed_disk" "managed_disk" {
-  for_each = local.create_additional_disks ? { for index, value in range(length(var.disk_sizes_in_gb)) : index => value } : {}
+  for_each = contains(["Linux"], var.vm_os_name) ? { vm = "Linux" } : {}
 
-  name                 = "${local.managed_disk_name_prefix}-${each.key}"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  storage_account_type = local.managed_disk_storage_account_type
-  create_option        = local.managed_disk_create_option
-  disk_size_gb         = each.value
-}
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  log_workspace_id    = var.log_workspace_id
 
-locals {
-  vm_disk_caching = "ReadWrite"
-}
+  vm_name = var.vm_name
+  vm_size = var.vm_size
 
-resource "azurerm_virtual_machine_data_disk_attachment" "vm_disk_attachment" {
-  for_each = local.create_additional_disks ? { for index in range(length(var.disk_sizes_in_gb)) : index => index } : {}
+  vm_admin_username                  = var.vm_admin_username
+  vm_admin_password                  = var.vm_admin_password
+  vm_disable_password_authentication = var.vm_disable_password_authentication
+  vm_admin_ssh_key                   = var.vm_admin_ssh_key
 
-  managed_disk_id    = azurerm_managed_disk.managed_disk[each.value].id
-  virtual_machine_id = azurerm_linux_virtual_machine.vm.id
-  lun                = (each.value + 1) * 10
-  caching            = local.vm_disk_caching
+  vm_nic_id = module.vm-network-interface.id
+
+  vm_source_image_reference = var.vm_source_image_reference
+  identity                  = var.identity
+
+  storage_account_type = var.storage_account_type
+  os_disk_caching      = var.os_disk_caching
+  disk_sizes_in_gb     = var.disk_sizes_in_gb
+
+  tags = var.tags
 
   depends_on = [
-    azurerm_linux_virtual_machine.vm
-  ]
-}
-
-locals {
-  diagnostics_name               = "${var.vm_name}-virtual-machine-diagnostics"
-  target_resource_id             = azurerm_linux_virtual_machine.vm.id
-  diagnostics_workspace_provided = var.log_workspace_id != null
-}
-
-module "diagnostics" {
-  source   = "github.com/ItayMayo/terraform-modules//diagnostic-settings"
-  for_each = local.diagnostics_workspace_provided ? { "1" : "1" } : {}
-
-  name                       = local.diagnostics_name
-  target_resource_id         = local.target_resource_id
-  log_analytics_workspace_id = var.log_workspace_id
-
-  depends_on = [
-    azurerm_linux_virtual_machine.vm
+    module.vm-network-interface
   ]
 }
