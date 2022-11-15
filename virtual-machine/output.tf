572272ebd7fdd81863d46575dbf72ea6e69e0de0
ItayMayo <itaymayo02@gmail.com>
2022-11-15 15:13:06 +0200

    virtual-machine: output.tf - Updated outputs.

--- virtual-machine/output.tf
+++ virtual-machine/output.tf
@@ -1,19 +1,24 @@
 output "id" {
-  value       = azurerm_linux_virtual_machine.vm.id
+  value       = try(module.windows-vm.id, module.linux-vm.id)
   description = "Virtual Machine resource id."
 }
 
 output "name" {
-  value       = azurerm_linux_virtual_machine.vm.name
+  value       = try(module.windows-vm.name, module.linux-vm.name)
   description = "Virtual Machine resource name."
 }
 
 output "object" {
-  value       = azurerm_linux_virtual_machine.vm
+  value       = contains(["Windows"], var.vm_os_name) ? module.windows-vm : module.linux-vm
   description = "Virtual Machine resource object."
 }
 
 output "nic_object" {
   value       = module.vm-network-interface.object
   description = "Network Interface resource object associated with the Virtual Machine."
+}
+
+output "vm_os_name" {
+  value       = var.vm_os_name
+  description = "Name of the Operating System for the Virtual Machine."
 }
